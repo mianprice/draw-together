3b37a090875bdb986f22d5861df70f56bc67d330
Ian Price <michael.ian.price@gmail.com>
2017-04-07 11:19:01 -0400

    Initial project setup complete

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', (req,res,next) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log(`[Socket ID: ${socket.id}] has connected.`);
+  socket.on('disconnect', () => {
+    console.log(`[Socket ID: ${socket.id}] has disconnected.`);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Started up.');
+});
