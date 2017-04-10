e2c01d62003acd465e54458c332a3a026313ecf2
Ian Price <michael.ian.price@gmail.com>
2017-04-09 22:55:25 -0400

    New sockets get sent the current state of the canvas upon connection to the server.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+var collection = [];
+
 app.use(express.static('public'));
 
 app.get('/', (req,res,next) => {
@@ -11,10 +13,19 @@ app.get('/', (req,res,next) => {
 
 io.on('connection', (socket) => {
   console.log(`[Socket ID: ${socket.id}] has connected.`);
+  collection.forEach((obj) => {
+    io.sockets.connected[socket.id].emit(obj.type, obj.draw_coord, obj.brush_color, obj.brush_width);
+  });
   socket.on('disconnect', () => {
     console.log(`[Socket ID: ${socket.id}] has disconnected.`);
   });
   socket.on('draw', (arr, color, width) => {
+    collection.push({
+      "type": 'draw',
+      "draw_coord": arr,
+      "brush_color": color,
+      "brush_width": width
+    });
     socket.broadcast.emit('draw', arr, color, width);
   });
 });
